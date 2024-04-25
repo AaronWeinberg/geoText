27c7f74993586eae7a6151e822b8fc3c0820c846
Aaron Weinberg <aaron.weinberg@gmail.com>
2024-04-24 22:04:54 -0500

    fix module errors

--- storySaver.sh
+++ storySaver.sh
@@ -8,7 +8,8 @@ tmux split-window -h -p 33
 tmux split-window -v -t 1 -p 66;
 
 ## shell (-t 0)
-tmux send-keys -t 0 'code .' Enter;
+# tmux send-keys -t 0 'code .' Enter; # open vscode
+tmux send-keys -t 0 'hx .' Enter; # open helix
 tmux send-keys -t 0 'clear' Enter;
 
 ## typescript watcher (-t 1)
